10e54a7f4b1ce277cc9e3c47ddad9c2b4ba35d36
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-03-18 20:54:49 -0300

    My first commit

---
+++ src/main/java/com/academiamaster/AcademiaMasterApplication.java
@@ -0,0 +1,13 @@
+package com.academiamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academiamaster/AcademiaMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academiamaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

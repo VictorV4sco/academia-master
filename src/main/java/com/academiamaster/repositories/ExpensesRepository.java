ab8064e4aac78a3e164aefd98f4a2e864574c4a7
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-04-12 18:30:33 -0300

    FindByDay, Month, year - Expenses methods

--- src/main/java/com/academiamaster/repositories/ExpensesRepository.java
+++ src/main/java/com/academiamaster/repositories/ExpensesRepository.java
@@ -1,9 +1,23 @@
 package com.academiamaster.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.academiamaster.entities.Expenses;
 
 public interface ExpensesRepository extends JpaRepository<Expenses, Long>{
 	
+	@Query("SELECT e FROM Expenses e WHERE FUNCTION('YEAR', e.moment) = :year")
+	Page<Expenses> findByYear(@Param("year") Integer year, Pageable pageable);
+	
+	@Query("SELECT e FROM Expenses e WHERE FUNCTION('MONTH', e.moment) = :month AND FUNCTION('YEAR', e.moment) = :year")
+	Page<Expenses> findByMonth(@Param("month") Integer month, @Param("year") Integer year, Pageable pageable);
+	
+	@Query("SELECT e FROM Expenses e WHERE FUNCTION('MONTH', e.moment) = :month AND FUNCTION('YEAR', e.moment) = :year AND FUNCTION('DAY', e.moment) = :day")
+	List<Expenses> findByDay(@Param("month") Integer month, @Param("year") Integer year, @Param("day") Integer day);
 }
--- src/main/java/com/academiamaster/services/ExpensesService.java
+++ src/main/java/com/academiamaster/services/ExpensesService.java
@@ -1,6 +1,11 @@
 package com.academiamaster.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,26 +21,29 @@ public class ExpensesService {
 	private ExpensesRepository expensesRepository;
 	
 	@Transactional(readOnly = true)
-	public ExpensesDTO findById(Long id) {
+	public ExpensesDTO findExpensesById(Long id) {
 		Expenses expenses = expensesRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
 		return new ExpensesDTO(expenses.getId(), expenses.getType(), expenses.getName(), expenses.getValue(), expenses.getMoment());
 	}
 	
 	@Transactional(readOnly = true)
-	public ExpensesDTO findByYear(Integer year) {
-		
-		return null;
+	public Page<ExpensesDTO> findPaymentsPagedByYear(Integer year, Pageable pageable) {
+		Page<Expenses> e = expensesRepository.findByYear(year, pageable);
+		return e.map(ex -> new ExpensesDTO(ex.getId(), ex.getType(), ex.getName(), ex.getValue(), ex.getMoment()));
 	}
 	
 	@Transactional(readOnly = true)
-	public ExpensesDTO findByMonth(Integer month) {
-		
-		return null;
+	public Page<ExpensesDTO> findExpensesPagedByMonth(Integer month, Integer year, Pageable pageable) {
+		Page<Expenses> e = expensesRepository.findByMonth(year, month, pageable);
+		return e.map(ex -> new ExpensesDTO(ex.getId(), ex.getType(), ex.getName(), ex.getValue(), ex.getMoment()));
 	}
 	
 	@Transactional(readOnly = true)
-	public ExpensesDTO findByDay(Integer day) {
-		
-		return null;
+	public List<ExpensesDTO> findExpensesListedByDay(Integer year, Integer month, Integer day) {
+		List<Expenses> e = expensesRepository.findByDay(month, year, day);
+		if(e.size() == 0) {
+			throw new ResourceNotFoundException("No payments on this day");
+		}
+		return e.stream().map(ex -> new ExpensesDTO(ex.getId(), ex.getType(), ex.getName(), ex.getValue(), ex.getMoment())).collect(Collectors.toList());
 	}
 }
